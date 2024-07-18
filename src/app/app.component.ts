26859bbb6ea52f7e0da3850212a815004029c990
AdamBoczula <adam.boczula@hotmail.com>
2024-07-18 10:45:13 +0200

    Signal input model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,43 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, effect, signal } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ApplicationSidebarComponent } from './application-sidebar/application-sidebar.component';
+import { MenuModel } from './menu.model';
+import { faBomb, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ApplicationSidebarComponent],
-  template: `<h1>{{ title() }}</h1>
-    <p>You are here {{ duration() }} seconds.</p>
-    <p>==> {{ durationInHrs() | number: '1.2-2' }} minutes.</p>
-    <app-application-sidebar *ngIf="duration() > 3 && duration() < 10" /> `,
+  imports: [CommonModule, ApplicationSidebarComponent, FontAwesomeModule],
+  template: `
+    <div class="app-container">
+      <app-application-sidebar
+        [(isOpened)]="isMenuOpen"
+        [menuModel]="menuModel"
+      />
+
+      <div class="app-content">
+        <h1>{{ title() }}</h1>
+        <p>You are here {{ duration() }} seconds.</p>
+        <p>==> {{ durationInHrs() | number: '1.2-2' }} minutes.</p>
+
+        <hr />
+
+        <button (click)="expandMenu()">Expand menu</button>
+      </div>
+    </div>
+  `,
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   public title = signal('Angular with Signal.');
   public duration = signal(0);
   public durationInHrs = computed(() => this.duration() / 60);
+  public readonly menuModel: MenuModel[] = [
+    { text: 'Home', icon: faHouse },
+    { text: 'Cool things', icon: faBomb },
+  ];
+  public isMenuOpen = signal(false);
   public intervalHandler;
   public timeoutHandler;
 
@@ -37,6 +59,10 @@ export class AppComponent {
       const seconds = localStorage.getItem('timeInSec');
       console.log('🍕 Logger function for duration: ', seconds);
     });
+  }
+
+  public expandMenu(): void {
+    this.isMenuOpen.set(true);
   }
 
   public ngOnDestroy() {
--- src/app/application-sidebar/application-sidebar.component.ts
+++ src/app/application-sidebar/application-sidebar.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, model } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MenuModel } from '../menu.model';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-application-sidebar',
   standalone: true,
-  imports: [],
-  template: ` <p>Application sidebar works!</p> `,
+  imports: [CommonModule, FontAwesomeModule],
+  template: `
+    <div
+      [ngClass]="{
+        'menu-container': true,
+        expand: isOpened(),
+        collapsed: !isOpened(),
+      }"
+    >
+      <div class="menu-item-wrapper" *ngFor="let menuItem of menuModel()">
+        <fa-icon [icon]="menuItem.icon"></fa-icon>
+        <p *ngIf="isOpened()">{{ menuItem.text }}</p>
+      </div>
+      <div class="expand-icon-wrapper" (click)="toggleMenu()">
+        <fa-icon [ngClass]="{ rotate180: isOpened() }" [icon]="expandIcon" />
+      </div>
+    </div>
+  `,
   styleUrl: './application-sidebar.component.scss',
 })
 export class ApplicationSidebarComponent {
-  public ngOnDestroy() {
-    console.log('🚀 ~ ApplicationSidebarComponent ~ ngOnDestroy ');
+  isOpened = model.required<boolean>();
+  menuModel = input.required<MenuModel[]>();
+  expandIcon = faAnglesRight;
+
+  public toggleMenu(): void {
+    this.isOpened.update((prev) => !prev);
   }
 }
